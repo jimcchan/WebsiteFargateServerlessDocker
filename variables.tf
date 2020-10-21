a4c6c0918ec0dc067589779603226d6130e534f3
jimcchan <35351395+jimcchan@users.noreply.github.com>
2020-10-21 16:46:33 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+     description = "Access key to AWS console"
+     default = "AKIAIOSFODNN7EXAMPLE"
+ }
+
+variable "secret_key" {
+     description = "Secret key to AWS console"
+     default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY"
+ }
